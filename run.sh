2a87fb0f8287b8c0bebf7fe4d04b66a34bf753f5
Stefan Zier <stefan@sumologic.com>
2014-12-10 17:02:20 -0800

    Fixing new run.sh script and port mapping in the README.

--- run.sh
+++ run.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-if [ $# = 2 ]; then
-	if [ -z "$SUMO_ACCESS_ID" ]; then
-		SUMO_ACCESS_ID="$1"
-	fi
+access_id=${SUMO_ACCESS_ID:=$1}
+access_key=${SUMO_ACCESS_KEY:=$2}
+collector_name=${SUMO_COLLECTOR_NAME:=collector_container}
+sources_json=${SUMO_SOURCES_JSON:=/etc/sumo-sources.json}
 
-	if [ -z "$SUMO_ACCESS_KEY" ]; then
-		SUMO_ACCESS_KEY="$2"
-	fi
-fi
-
-if [ -z "$SUMO_ACCESS_ID" ] || [ -z "$SUMO_ACCESS_KEY" ]; then
-	echo "Please provide credentials, either via the SUMO_ACCESS_ID and SUMO_ACCESS_KEY environment variables,"
-	echo "or as the first two command line arguments!"
+if [ -z "$access_id" ] || [ -z "$access_key" ]; then
+	echo "FATAL: Please provide credentials, either via the SUMO_ACCESS_ID and SUMO_ACCESS_KEY environment variables,"
+	echo "       or as the first two command line arguments!"
 	exit 1
 fi
 
-/opt/SumoCollector/collector console -- -t -i $SUMO_ACCESS_ID -k $SUMO_ACCESS_KEY -n ${SUMO_COLLECTOR_NAME:=collector_container} -s ${SUMO_SOURCES_JSON:=/etc/sumo_sources.json}
+if [ ! -e $sources_json ]; then
+	echo "FATAL: Unable to find $sources_json - please make sure you include it in your image!"
+	exit 1
+fi
+
+/opt/SumoCollector/collector console -- -t -i $access_id -k $access_key -n $collector_name -s $sources_json
