88c33b8a69bb512594d2b7f4a307c3a3177a7fed
Sebastian Otaegui <feniix@gmail.com>
2016-07-20 21:30:58 -0300

    make memory and other settings configurable

--- Dockerfile
+++ Dockerfile
@@ -4,16 +4,21 @@
 FROM ubuntu:14.04
 MAINTAINER Sumo Logic <docker@sumologic.com>
 
-ENV DEBIAN_FRONTEND noninteractive
-RUN apt-get update --quiet && \
- apt-get upgrade --quiet --force-yes -y && \
- apt-get install --quiet --force-yes -y wget && \
- wget -q -O /tmp/collector.deb https://collectors.sumologic.com/rest/download/deb/64 && \
- dpkg -i /tmp/collector.deb && \
- rm /tmp/collector.deb && \
- apt-get remove --quiet --force-yes -y wget && \
- apt-get clean --quiet && \
- rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+RUN \
+  DEBIAN_FRONTEND=noninteractive apt-get update --quiet && \
+  DEBIAN_FRONTEND=noninteractive apt-get upgrade --quiet --force-yes -y && \
+  DEBIAN_FRONTEND=noninteractive apt-get install --quiet --force-yes -y wget && \
+  wget -q -O /tmp/collector.deb https://collectors.sumologic.com/rest/download/deb/64 && \
+  dpkg -i /tmp/collector.deb && \
+  rm /tmp/collector.deb && \
+  DEBIAN_FRONTEND=noninteractive apt-get remove --quiet --force-yes -y wget && \
+  DEBIAN_FRONTEND=noninteractive apt-get clean --quiet && \
+  rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
-COPY run.sh run.sh 
-ENTRYPOINT ["/bin/bash", "run.sh"]
+ENV COLLECTOR_MEM 128
+ENV WRAPPER_DEBUG FALSE
+ENV LOG_TO_STDOUT FALSE
+
+COPY run.sh run.sh
+
+CMD ["/bin/bash", "run.sh"]
--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -x
 
 access_id=${SUMO_ACCESS_ID:=$1}
 access_key=${SUMO_ACCESS_KEY:=$2}
@@ -7,14 +9,23 @@ collector_name=${SUMO_COLLECTOR_NAME:=collector_container}
 sources_json=${SUMO_SOURCES_JSON:=/etc/sumo-sources.json}
 
 if [ -z "$access_id" ] || [ -z "$access_key" ]; then
-	echo "FATAL: Please provide credentials, either via the SUMO_ACCESS_ID and SUMO_ACCESS_KEY environment variables,"
-	echo "       or as the first two command line arguments!"
-	exit 1
+  echo "FATAL: Please provide credentials, either via the SUMO_ACCESS_ID and SUMO_ACCESS_KEY environment variables,"
+  echo "       or as the first two command line arguments!"
+  exit 1
 fi
 
-if [ ! -e $sources_json ]; then
-	echo "FATAL: Unable to find $sources_json - please make sure you include it in your image!"
-	exit 1
+if [ ! -e "$sources_json" ]; then
+  echo "FATAL: Unable to find $sources_json - please make sure you include it in your image!"
+  exit 1
+fi
+
+sed -i "s/wrapper.java.initmemory=.*/wrapper.java.initmemory=$COLLECTOR_MEM/g" /opt/SumoCollector/config/wrapper.conf
+sed -i "s/wrapper.java.maxmemory=.*/wrapper.java.maxmemory=$COLLECTOR_MEM/g" /opt/SumoCollector/config/wrapper.conf
+if [ "$WRAPPER_DEBUG" == "TRUE" ]; then
+  sed -i "s/#\ wrapper.debug=.*/wrapper.debug=TRUE/g" /opt/SumoCollector/config/wrapper.conf
+fi
+if [ "$LOG_TO_STDOUT" == "TRUE" ]; then
+  sed -i "s/wrapper.logfile=.*/wrapper.logfile=/dev/stdout" /opt/SumoCollector/config/wrapper.conf
 fi
 
 /opt/SumoCollector/collector console -- -t -i $access_id -k $access_key -n $collector_name -s $sources_json -u $receiver_url
