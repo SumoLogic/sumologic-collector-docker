b8277f85f2ddc2489e28d97748a8d03bfdb337fc
Nari <smulakala@salesforce.com>
2018-02-15 11:16:36 -0800

    fix indentation issue

--- sfiq/get_key.py
+++ sfiq/get_key.py
@@ -9,12 +9,11 @@ def get_key():
 	crypter_env = os.environ.get('CRYPTER_ENV', 'ops')
 	crypter_key_for_sumo_key_id = os.environ.get('CRYPTER_KEY_FOR_SUMO_KEY_ID', 'SUMO_KEY_ID')
 	crypter_key_for_sumo_key_secret = os.environ.get('CRYPTER_KEY_FOR_SUMO_KEY_SECRET', 'SUMO_KEY_SECRET')
-
-    cc = Client(crypter_role, crypter_env=crypter_env)
-    sumo_key_id = cc.read_credential(crypter_key_for_sumo_key_id)
-    sumo_key_secret = cc.read_credential(crypter_key_for_sumo_key_secret)
-    return sumo_key_id, sumo_key_secret
+	cc = Client(crypter_role, crypter_env=crypter_env)
+	sumo_key_id = cc.read_credential(crypter_key_for_sumo_key_id)
+	sumo_key_secret = cc.read_credential(crypter_key_for_sumo_key_secret)
+	return sumo_key_id, sumo_key_secret
 
 if __name__ == '__main__':
-    key_id, key_secret = get_key()
-    print '%s:%s' % (key_id, key_secret)
+	key_id, key_secret = get_key()
+	print '%s:%s' % (key_id, key_secret)
